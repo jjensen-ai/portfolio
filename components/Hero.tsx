472d91ce46d9a9ad2f951e6e78673a675bd398f6
jjensen-ai <jordon.jensen@live.ca>
2023-05-12 11:42:57 -0400

    added background image, worked on scale

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,14 +2,15 @@ import styles from '@/styles/hero.module.css';
 
 export default function Hero() {
   return (
-    <div className='flex flex-col w-full h-[88.2vh] relative'>
+    <div
+      className={`flex flex-col w-full h-[43rem] relative ${styles.displayPic}`}>
       <div className='flex justify-end mt-[2rem]'>
-        <div className='absolute right-0 mt-4'>
+        <div className='absolute right-0 pr-5 mt-4'>
           <h1 className={`${styles.tagLine}`}>Developer</h1>
           <h1 className={`${styles.tagLine}`}>UX UI Enthusiast</h1>
         </div>
       </div>
-      <div className='flex flex-col absolute bottom-0 pl-8'>
+      <div className='flex flex-col absolute bottom-16 pl-12'>
         <span className={`${styles.name} tracking-wide `}>Jordon</span>
         <span className={`${styles.name} tracking-wide`}>Jensen</span>
       </div>
